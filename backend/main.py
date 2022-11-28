b638a496378b0c14ffc68199c85fffeddff34ca9
Антон <2881670@mail.ru>
2022-11-28 17:10:50 +0300

    Rout Navbar

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+def include_router(app) -> None:
+	app.include_router(general_pages_router)
+
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+	include_router(app)
+	return app
 
 
-@app.get("/")
-def hello_api():
-    return {"msg": "Hello API"}
+app = start_application()
